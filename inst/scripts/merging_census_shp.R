aaf0d3f2e71c35c13790f68d28f1a554a5daa7d5
dasalm20 <dasalm20@gmail.com>
2022-03-23 16:38:10 -0400

    code merging the shp files at the Bairro level
    This is an updated code in which I created the IDs and merged the files. There are still issues with teh final merged file.

--- inst/scripts/merging_census_shp.R
+++ inst/scripts/merging_census_shp.R
@@ -10,18 +10,35 @@
 
 #1. Loading census data which are .dta file
 #library(haven)
+.libPaths("E:/Daylan/R")
+library(sf)
 library(readstata13)
 library(data.table)
+library(dplyr)
+library(tidyverse)
+library (ggplot2)
+library(rgdal)
+
 
 #Load The data
-census_full <- readstata13::read.dta13("D:/AFR_Database/SSAPOV-Harmonization/Daylan/GMD/MOZ_Census/01.Input/MOZ Pop Census 2017 Full (selected vars).dta")
+
+
+##census_full <- readstata13::read.dta13("D:/AFR_Database/SSAPOV-Harmonization/Daylan/GMD/MOZ_Census/01.Input/MOZ Pop Census 2017 Full (selected vars).dta")
+
+
+##For the 10% work, see line 78
+census_10 <- readstata13::read.dta13("D:/AFR_Database/SSAPOV-Harmonization/Daylan/GMD/MOZ_Census/01.Input/MOZ Pop Census 2017_10 percent.dta")
 
 
 moz_shp <- sf::st_read(dsn = "//esapov/esapov/MOZ/GEO/Population/moz_censusshapefile2017",
                        layer = "BASE_COMPLETA_DE_AE_CENSO_2017")
 
+crs_dt<- rgdal::make_EPSG()
+moz_shp<- st_transform(moz_shp, crs_dt$prj4[crs_dt$code==3974])
+rm(crs_dt)
 
-census_full$PROVINCIA<- as.integer(census_full$PROVINCIA)
+
+# census_full$PROVINCIA<- as.integer(census_full$PROVINCIA)
 
 ##Keep only the relevant variables from the census_full. (ie. Labour informality and identifiers)
 
@@ -29,18 +46,63 @@ census_full$PROVINCIA<- as.integer(census_full$PROVINCIA)
 ##This function adds a leading zero for variables with 2 9similar to the variables in moz_shp
 
 ad_zero02<- function(X){
-  return(sprintf("%02d",X))
+  return(sprintf("%03d",X))
 }
 
-##Apply the funtion to each variable in census_full
+#
+# ##Apply the funtion to each variable in census_full
+#
+# new_vars<-apply(census_full[,c("PROVINCIA","DISTRITO","POSTO","LOCALIDADE",
+#                                    "BAIRRO")],
+#                 MARGIN = 2,FUN = ad_zero02)
+#
+# census_full$AE<-sprintf("%3d",census_full$AE)
+#
+# ##Since the Apply function returns a matrix, we convert it into a data frame
+#
+# new_vars<- as.data.frame(new_vars)
+#
+# ##Now we change the name of the columns
+#
+#
+# colnames(new_vars)<- c('Province',"District","Postal", "Location","Neighbor")
+#
+#
+# ##Merge our our new colums with the census.
+#
+# Census_merged <- cbind(new_vars,census_full)
+# rm(new_vars,census_full)
+#
+#
+# ##Create the AES variable to merge with the shp file.
+#
+# Census_merged$AES<- paste0(Census_merged$Province,Census_merged$District,Census_merged$Postal,
+#                            Census_merged$Location,Census_merged$Neighbor,Census_merged$AE)
+#
+# ##Merge
+#
+# Maputo_merged_data <- merge(moz_shp,Census_merged, by="AES")
+# rm(Census_merged,moz_shp)
+#
+#
+#
 
-new_vars<-apply(census_full[,c("PROVINCIA","DISTRITO","POSTO","LOCALIDADE",
-                                   "BAIRRO")],
+##Work on the 10% census
+
+##Unique ID creation. This ID does not match the shp file.
+census_10$Cod_distrito<- as.character(census_10$Cod_distrito)
+census_10$Cod_AF_Distrito <- sprintf("%014.0f",census_10$Cod_AF_Distrito)
+census_10$AES<- paste0(census_10$Cod_distrito,census_10$Cod_AF_Distrito)
+
+
+
+#Alternative ID to merge with the shp file
+census_10$A2_PROVINCIA<- as.integer(census_10$A2_PROVINCIA)
+
+new_vars<-apply(census_10[,c("A2_PROVINCIA","A3_DISTRITO","POSTO","LOCALIDADE",
+                               "BAIRRO")],
                 MARGIN = 2,FUN = ad_zero02)
 
-census_full$AE<-sprintf("%03d",census_full$AE)
-
-##Since the Apply function returns a matrix, we convert it into a data frame
 
 new_vars<- as.data.frame(new_vars)
 
@@ -50,24 +112,110 @@ new_vars<- as.data.frame(new_vars)
 colnames(new_vars)<- c('Province',"District","Postal", "Location","Neighbor")
 
 
-##Merge our our new colums with the census.
-
-Census_merged <- cbind(new_vars,census_full)
-rm(new_vars,census_full)
+##Merge our our new col with the census.
 
 
-##Create the AES variable to merge with the shp file.
+census_10 <- cbind(new_vars,census_10)
+rm(new_vars)
 
-Census_merged$AES<- paste0(Census_merged$Province,Census_merged$District,Census_merged$Postal,
-                           Census_merged$Location,Census_merged$Neighbor,Census_merged$AE)
+census_10$ID<- paste0(census_10$Province,census_10$District,census_10$Postal,
+                      census_10$Location,census_10$Neighbor)
 
-##Merge
+length(unique(census_10$ID))
 
-Maputo_merged_data <- merge(moz_shp,Census_merged, by="AES")
-rm(Census_merged,moz_shp)
+##Subsetthe census
+census_10<- as.data.table(census_10)
+census_10<-census_10[,.(ID,TIPO_DE_TRABALHADOR,HP31,NIVEL_DE_ENSINO_FREQUENTADO,P24_NIVEL_DE_ENSINO_CONCLUIDO_1,
+                        CURSO_SUPERIOR_CONCLUIDOX,CURSO_SUPERIOR_CONCLUIDO,TRABALHOU_NA_ULTIMA_SEMANA_JULHO,
+                        PORQUE_NAO_TRABALHOU,TRABALHO_DOMESTICO,HORAS_TRABALHADAS_ULTIMA_SEMANA)]
 
-##Check the merge
 
-unique(length(Maputo_merged_data$AES))
+
+
+
+##Create the same ID on the shp file
+moz_shp$CodProv<- as.integer(moz_shp$CodProv)
+moz_shp$CodDist<- as.integer(moz_shp$CodDist)
+moz_shp$CodPost<- as.integer(moz_shp$CodPost)
+moz_shp$CodLocal<- as.integer(moz_shp$CodLocal)
+moz_shp$CodBairro<- as.integer(moz_shp$CodBairro)
+
+
+
+
+
+##Know what distric code corresponds to what distric name
+moz_shp<- as.data.table(moz_shp)
+
+moz_shp[Distrito == "AEROPORTO INTERNACIONAL DE MAPUTO", CodDist := 24]
+
+test<-unique(moz_shp[,c("CodDist", "Distrito")])
+rm(test)
+
+new_vars<-apply(moz_shp[,c("CodProv","CodDist","CodPost","CodLocal",
+                             "CodBairro")],
+                MARGIN = 2,FUN = ad_zero02)
+
+
+new_vars<- as.data.frame(new_vars)
+
+##Now we change the name of the columns
+
+colnames(new_vars)<- c('Province',"District","Postal", "Location","Neighbor")
+
+
+moz_shp1 <- cbind(new_vars,moz_shp)
+rm(new_vars)
+
+moz_shp$ID<- paste0(moz_shp1$Province,moz_shp1$District,moz_shp1$Postal,
+                    moz_shp1$Location,moz_shp1$Neighbor)
+rm(moz_shp1)
+
+length(unique(moz_shp$ID))
+
+
+####Create a shpfile at the barrio level
+moz_shp <- st_as_sf(moz_shp, agr="constant" )
+
+moz_shp<-moz_shp %>%
+          group_by(ID) %>%
+          summarize()
+
+
+
+
+moz_shp1<- moz_shp
+
+
+
+
+## Merge the data
+
+
+moz_shp <- merge(census_10,moz_shp, by="ID")
+
+moz_shp <- st_as_sf(moz_shp, agr="constant",crs=3974 )
+
+
+## 1720 unique observations at the bairro level for the shp.
+
+#1715 unique observations from the 10 census.
+
+## 1705 unique observations on the merged shp
+
+
+
+plot(st_geometry(moz_shp))
+
+
+
+
+
+
+
+
+sf::st_write(obj = moz_shp, dsn = "//esapov/esapov/MOZ/GEO/Population/Merged_shp",
+                                                          layer = "Moz_merged_shp_10",
+                                                          driver = "ESRI Shapefile", append = FALSE)
 
 
